546275fb65b40601d80d78dce8acfa349ac4526d
Kaustubh Ladiya <k4ustu3h@gmail.com>
2025-08-02 22:40:53 +0530

    Add Account Activity
    Updated
      - Move AppBar out of MenuFragment to the MainActivity
      - AGP

---
+++ app/src/main/java/k4ustu3h/pizzatogo/AccountActivity.kt
@@ -0,0 +1,53 @@
+package k4ustu3h.pizzatogo
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import k4ustu3h.pizzatogo.databinding.ActivityAccountBinding
+
+class AccountActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAccountBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityAccountBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setSupportActionBar(binding.topAppBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val user: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+
+        binding.email.text = user?.email
+
+        binding.logout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+
+            finish()
+        }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
+    }
+}
--- app/src/main/java/k4ustu3h/pizzatogo/MainActivity.kt
+++ app/src/main/java/k4ustu3h/pizzatogo/MainActivity.kt
@@ -1,5 +1,6 @@
 package k4ustu3h.pizzatogo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -24,6 +25,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.account -> {
+                    val intent = Intent(this, AccountActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+            }
+            true
         }
 
         setCurrentFragment(MenuFragment())
